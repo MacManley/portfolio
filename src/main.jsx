df5c55844fe7ec9e41b2a2c9dd8288004cd276c6
MacManley <manleyn@icloud.com>
2026-06-27 11:34:47 +0100

    Quick routing fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter, HashRouter } from 'react-router-dom';
 
+const redirectPath = window.sessionStorage.getItem('redirectPath');
+
+if (redirectPath) {
+  window.sessionStorage.removeItem('redirectPath');
+  window.history.replaceState(null, '', redirectPath);
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
